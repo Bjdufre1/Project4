e22bf16cfd55a1642aded260dac6964d1c32e572
aaron <amkowals@svsu.edu>
2018-04-23 17:47:32 -0400

    Read output.txt line by line

--- src/Project4.java
+++ src/Project4.java
@@ -1,8 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Project4 {
 
     public static void main(String[] args) {
 
+        File analyzerOutput = new File("output.txt");
 
+        try (Scanner inFile = new Scanner(analyzerOutput)) {
+
+            while (inFile.hasNextLine()) {
+                System.out.println(inFile.next());
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
 
