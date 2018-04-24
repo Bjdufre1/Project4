5ffe370fbae6e376838a8761748380261f9d7f49
Brady Dufresne <bjdufre1@svsu.edu>
2018-04-23 22:35:40 -0400

    Finished example working correctly

--- src/Project4.java
+++ src/Project4.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.Scanner;
 
 public class Project4 {
 
@@ -7,6 +6,7 @@ public class Project4 {
     public static int rightParenCount = 0;
     public static int leftCurlyCount = 0;
     public static int rightCurlyCount = 0;
+    public static boolean hasEnd = false;
     public static BufferedReader br;
 
     public static void main(String[] args) throws IOException, ParserError {
@@ -48,138 +48,137 @@ public class Project4 {
                 br.readLine();
             }
             if(codeOfWord == 41){
-                switch (word){
+                switch (word) {
 
                     case ("int"): {
-                        if(!(Integer.parseInt(br.readLine()) == 11)){
+                        if (!(Integer.parseInt(br.readLine()) == 11)) {
                             throw new ParserError("int must be followed by an identifier");
                         }
                         br.readLine();
-                        if(!(Integer.parseInt(br.readLine()) == 27)){
+                        if (!(Integer.parseInt(br.readLine()) == 27)) {
                             throw new ParserError("int must be assigned on declaration");
                         }
                         br.readLine();
                         int expressionNext = Integer.parseInt(br.readLine());
-                        if(expressionNext == 10 || expressionNext == 11) {
-                            if(expressionNext == 11){
+                        if (expressionNext == 10 || expressionNext == 11) {
+                            if (expressionNext == 11) {
                                 br.readLine();
                                 int operator = Integer.parseInt(br.readLine());
-                                if(operator != 21 && operator != 22 && operator != 23 && operator != 24){
-                                    throw new ParserError("incorrect int assignment");
+                                if (operator != 21 && operator != 22 && operator != 23 && operator != 24) {
+                                    throw new ParserError("Incorrect int assignment");
                                 }
                                 br.readLine();
                                 expressionNext = Integer.parseInt(br.readLine());
-                                if(!(expressionNext == 10 || expressionNext == 11)){
-                                    throw new ParserError("incorrect int assignment");
+                                if (!(expressionNext == 10 || expressionNext == 11)) {
+                                    throw new ParserError("Incorrect int assignment");
                                 }
                                 br.readLine();
-                            }
-                            else {
+                            } else {
                                 br.readLine();
                             }
-                        }
-                        else {
+                        } else {
                             throw new ParserError("Incorrect assignment for int type");
                         }
-                        if(!(Integer.parseInt(br.readLine()) == 36)){
-                            throw new ParserError("missing semicolon after assignment");
+                        if (!(Integer.parseInt(br.readLine()) == 36)) {
+                            throw new ParserError("Missing semicolon after assignment");
                         }
                         br.readLine();
                         break;
 
                     }
                     case ("pnt"): {
-                        if(!(Integer.parseInt(br.readLine()) == 25)){
+                        if (!(Integer.parseInt(br.readLine()) == 25)) {
                             throw new ParserError("pnt must be followed by a left paren");
                         }
                         br.readLine();
                         leftParenCount++;
                         int code = Integer.parseInt(br.readLine());
-                        if(code == 39){
+                        if (code == 39) {
                             br.readLine();
                             code = Integer.parseInt(br.readLine());
                             br.readLine();
-                            while (code != 39){
+                            while (code != 39) {
                                 code = Integer.parseInt(br.readLine());
                                 br.readLine();
                             }
                             br.mark(1000);
-                            if(Integer.parseInt(br.readLine()) == 45){
+                            if (Integer.parseInt(br.readLine()) == 45) {
                                 br.readLine();
                                 int hold = Integer.parseInt(br.readLine());
-                                if(!(hold == 11 || hold == 39)){
+                                if (!(hold == 11 || hold == 39)) {
                                     throw new ParserError("Incorrect assignment for string");
                                 }
                                 br.readLine();
-                            }
-                            else {
+                            } else {
                                 br.reset();
                             }
-                        }
-                        else if(code == 11){
+                        } else if (code == 11) {
                             br.readLine();
                             br.mark(1000);
-                            if(Integer.parseInt(br.readLine()) == 45){
+                            if (Integer.parseInt(br.readLine()) == 45) {
                                 br.readLine();
                                 int hold = Integer.parseInt(br.readLine());
-                                if(!(hold == 11 || hold == 39)){
+                                if (!(hold == 11 || hold == 39)) {
                                     throw new ParserError("Incorrect assignment for string");
                                 }
                                 br.readLine();
-                            }
-                            else {
+                            } else {
                                 br.reset();
                             }
                         }
                         int num = Integer.parseInt(br.readLine());
-                        if(num == 26){
+                        if (num == 26) {
                             rightParenCount++;
                         }
                         br.readLine();
                         int number = Integer.parseInt(br.readLine());
-                        if(number != 36){
+                        if (number != 36) {
                             throw new ParserError("Not ended with semicolon");
                         }
                         br.readLine();
 
                         break;
                     }
-                    case ("if") :
-                        if(!(Integer.parseInt(br.readLine()) == 25)){
+                    case ("if"): {
+                        if (!(Integer.parseInt(br.readLine()) == 25)) {
                             throw new ParserError("if must be followed by a left paren");
                         }
                         br.readLine();
                         leftParenCount++;
                         int hold = Integer.parseInt(br.readLine());
-                        if(!(hold == 11 || hold == 10)){
+                        if (!(hold == 11 || hold == 10)) {
                             throw new ParserError("if statement must contain variable or literal");
                         }
                         br.readLine();
                         hold = Integer.parseInt(br.readLine());
-                        if(!(hold == 29 || hold == 30 || hold == 27)){
+                        if (!(hold == 29 || hold == 30 || hold == 27)) {
                             throw new ParserError("if statement must be less than, greater than, equals, or a boolean");
                         }
                         br.readLine();
                         hold = Integer.parseInt(br.readLine());
-                        if(!(hold == 11 || hold == 10)) {
+                        if (!(hold == 11 || hold == 10)) {
                             throw new ParserError("if statement must contain variable or literal after operand");
                         }
                         br.readLine();
                         int num = Integer.parseInt(br.readLine());
-                        if(num == 26){
+                        if (num == 26) {
                             rightParenCount++;
                         }
                         br.readLine();
                         num = Integer.parseInt(br.readLine());
-                        if(num == 31){
+                        if (num == 31) {
                             leftCurlyCount++;
-                        }
-                        else {
+                        } else {
                             throw new ParserError("if must end with a curly");
                         }
                         br.readLine();
+                        break;
+                    }
 
-
+                    case ("end"): {
+                        hasEnd = true;
+                        break;
+                    }
                 }
             }
         }
@@ -189,7 +188,10 @@ public class Project4 {
         if(rightParenCount != leftParenCount){
             throw new ParserError("Parenthesis do not match");
         }
-        System.out.println("No errors detected");
+        if(!hasEnd) {
+            throw new ParserError("No end statement");
+        }
+        System.out.println("Parser Check Passed");
     }
 
 }
