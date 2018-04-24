872a52eca2a977f8dede5e5826fffce6896ffd0f
Brady Dufresne <bjdufre1@svsu.edu>
2018-04-23 21:09:27 -0400

    Int finished

--- src/Project4.java
+++ src/Project4.java
@@ -28,6 +28,49 @@ public class Project4 {
             if(codeOfWord == 42){
                 br.readLine();
             }
+            if(codeOfWord == 41){
+                switch (word){
+
+                    case ("int"): {
+                        if(!(Integer.parseInt(br.readLine()) == 11)){
+                            throw new ParserError("int must be followed by an identifier");
+                        }
+                        br.readLine();
+                        if(!(Integer.parseInt(br.readLine()) == 27)){
+                            throw new ParserError("int must be assigned on declaration");
+                        }
+                        br.readLine();
+                        int expressionNext = Integer.parseInt(br.readLine());
+                        if(expressionNext == 10 || expressionNext == 11) {
+                            if(expressionNext == 11){
+                                br.readLine();
+                                int operator = Integer.parseInt(br.readLine());
+                                if(operator != 21 && operator != 22 && operator != 23 && operator != 24){
+                                    throw new ParserError("incorrect int assignment");
+                                }
+                                br.readLine();
+                                expressionNext = Integer.parseInt(br.readLine());
+                                if(!(expressionNext == 10 || expressionNext == 11)){
+                                    throw new ParserError("incorrect int assignment");
+                                }
+                                br.readLine();
+                            }
+                            else {
+                                br.readLine();
+                            }
+                        }
+                        else {
+                            throw new ParserError("Incorrect assignment for int type");
+                        }
+                        if(!(Integer.parseInt(br.readLine()) == 36)){
+                            throw new ParserError("missing semicolon after assignment");
+                        }
+                        br.readLine();
+
+                    }
+
+                }
+            }
         }
     }
 }
